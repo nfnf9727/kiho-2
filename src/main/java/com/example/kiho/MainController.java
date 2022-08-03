5132646f7339f736ba95b9eee3f0ba6482ccc8e2
nfnf9727 <nfnf.9727@gmail.com>
2022-08-03 17:27:11 +0900

    DB接続完了！

--- src/main/java/com/example/kiho/MainController.java
+++ src/main/java/com/example/kiho/MainController.java
@@ -1,6 +1,15 @@
 package com.example.kiho;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,11 +19,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
+	@Autowired
+    private JdbcTemplate jdbcTemplate;
 	
     @RequestMapping(path = "/")
     public String showIndex(Model model) {
     	System.out.println("OK");
+    	String sql = "SELECT * FROM user";
+        List<Map<String, Object>> list = jdbcTemplate.queryForList(sql);
+        System.out.println(list);
+        
     	model.addAttribute("userName", "しぶたに");
+        
         return "index";
     }
     
