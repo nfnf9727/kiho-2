b9c86a088e932403154a531a93d27d3441784bda
nfnf9727 <nfnf.9727@gmail.com>
2023-02-16 23:13:56 +0900

    AWSデプロイ

--- src/main/java/com/example/kiho/MainController.java
+++ src/main/java/com/example/kiho/MainController.java
@@ -1,4 +1,4 @@
-package com.example.controller;
+package com.example.kiho;
 
 import java.util.Random;
 
--- src/test/java/com/example/demo/kiho/Kiho2ApplicationTests.java
+++ src/test/java/com/example/demo/kiho/Kiho2ApplicationTests.java
@@ -1,9 +1,11 @@
-package com.example.demo;
+package com.example.demo.kiho;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.example.kiho.Kiho2Application;
+
+@SpringBootTest(classes = Kiho2Application.class)
 class Kiho2ApplicationTests {
 
 	@Test
